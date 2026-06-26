4f3326011ab5f3bef469899cac8d42bf1ae94547
Rishab Sing <rishabsingh9821@gmail.com>
2026-06-26 21:55:00 +0530

    Python Concept

---
+++ Python List Concept/Python_list_Question_Pratice.py
@@ -0,0 +1,16 @@
+# Create a list of five fruits and print it.
+fruit =  ["apple" , "grapes" , "banana" , "mango" , "kiwi"]
+print(fruit)
+
+# Print the first element.
+print(fruit[0])
+
+#Print the last element without using its index number directly
+print(fruit[-4])
+
+#Print the length of the list.
+print(len(fruit))
+
+#Print every element using a for loop.
+for i in fruit:
+    print(i)
